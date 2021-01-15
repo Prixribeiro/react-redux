89483929eece4754ce2a7eb4a3086791301ff529
Priscila Ribeiro <priscila@MacBook-Pro-de-Priscila.local>
2021-01-15 18:09:07 -0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './App.css';
+import { bindActionCreators } from 'redux';
+import { clickButton} from './actions';
+
+class App extends Component {
+  state = {
+    inputValue: ''
+  }
+
+  inputChange = event => {
+    this.setState({
+      inputValue: event.target.value
+    })
+  }
+
+  render() {
+
+    const {
+      clickButton,
+      newValue
+    } = this.props; 
+
+    const { inputValue} = this.state;
+
+    return (
+      <div className="App" style={{paddingTop: '10px' }}>
+        <h2>Meu Projeto: React com Redux</h2>
+        <input type='text' 
+          onChange={this.inputChange}
+          value={inputValue}
+          />
+        <button onClick={()=>clickButton(inputValue)}> 
+        Enviar 
+        </button>
+        <h3> {newValue} </h3>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = store => ({
+  newValue: store.clickState.newValue
+});
+
+const mapDispatchToProps = dispatch => 
+bindActionCreators({clickButton}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps) (App);
